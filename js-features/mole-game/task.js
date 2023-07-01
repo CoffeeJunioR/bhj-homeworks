0deb56d395ec1d9f5ab90e61516a5b5baebe11e1
CoffeeJunioR <superrr.den@list.ru>
2023-07-01 15:20:49 +0300

    Fixed

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,22 +1,32 @@
 function getHole(index) {
     return document.getElementById('hole' + index)
 };
-let hole = 0;
 let deadCounter = document.getElementById('dead');
 let lostCounter = document.getElementById('lost');
 let deadCount = 0;
 let lostCount = 0;
+function resetGame() {
+    lostCount = 0;
+    lostCounter.textContent = lostCount;
+    deadCount = 0
+    deadCounter.textContent = deadCount;
+}
 for (let i = 1; i < 10; i++) {
-    hole = getHole([i]);
-    if (hole.classList.contains( 'hole_has-mole' ) == true) {
-        hole.onclick = function() {
+    let hole = getHole([i]);
+    hole.onclick = function() {
+        if (hole.classList.contains('hole_has-mole')) {
             deadCount += 1;
             deadCounter.textContent = deadCount;
-        }
-     } else {
-        hole.onclick = function() {
+        } else {
             lostCount += 1;
             lostCounter.textContent = lostCount;
         }
+        if ((deadCount == 10) && (!(lostCount >= 5))) {
+            alert('Вы победили!');
+            resetGame();
+        } else if (lostCount >=5) {
+            alert('Вы проиграли :(');
+            resetGame();
+        }
     }
-}     
+}
