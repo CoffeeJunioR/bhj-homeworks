f5cccf844b324ff29c47cf5f0898d49edcf6344b
CoffeeJunioR <superrr.den@list.ru>
2023-10-02 21:49:13 +0300

    Some fixes

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,21 +1,23 @@
 const products = document.querySelector('.products')
+const cart = document.querySelector('.cart__products')
+const attrs = [];
+
 
 products.addEventListener('click', clickBtn);
 products.addEventListener('click', addCount);
 products.addEventListener('click', takeCount);
 
 function clickBtn(event) {
-    if (event.target.closest('.product__add')) {
-        const cart = document.querySelector('.cart__products')
+    if ((event.target.closest('.product__add'))) {
         const activeBtn = event.target;
         const img = activeBtn.closest('.product__controls').previousElementSibling;
-        console.log(img)
-        console.log(activeBtn.closest('.product__controls').firstElementChild.childNodes[3].childNodes[3])
-        cart.innerHTML = `<div class="cart__product" data-id="${activeBtn.closest('.product').dataset.id}">
-        <img class="cart__product-image" src="${img.src}">
-        <div class="cart__product-count">${activeBtn.closest('.product__controls').firstElementChild.childNodes[3].childNodes[3].textContent}</div>
-    </div>`
-        
+        const tempElem = document.createElement('div');
+        tempElem.classList.add('cart__product');
+        tempElem.setAttribute('data-id', `${activeBtn.closest('.product').dataset.id}`)
+        attrs.push(activeBtn.closest('.product').dataset.id)
+        tempElem.innerHTML = `<img class="cart__product-image" src="${img.src}">
+        <div class="cart__product-count">${activeBtn.closest('.product__controls').firstElementChild.childNodes[3].childNodes[3].textContent}</div>`
+        cart.append(tempElem);
     }
 }
 const count = document.querySelectorAll('.product__quantity-control')
@@ -27,12 +29,10 @@ function addCount(event) {
 }
 
 function takeCount(event) {
-        if ((event.target.closest('.product__quantity-value') == 1) && (event.target.closest('.product__quantity-value'))) {
-            return;
-        
+        if ((event.target.closest('.product__quantity-controls').firstElementChild.nextElementSibling.textContent <= '1') && (event.target.closest('.product__quantity-control_dec'))) {
+            return
         }
         else if (event.target.closest('.product__quantity-control_dec')) {
-            console.log(event.target.closest('.product__quantity-control_dec').nextElementSibling.textContent)
             event.target.nextElementSibling.textContent = parseInt(event.target.nextElementSibling.textContent - 1)
     }
 }
