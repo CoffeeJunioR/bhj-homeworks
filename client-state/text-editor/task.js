c09c69fc753c1f2e0618a44d2ef5996495f04260
CoffeeJunioR <superrr.den@list.ru>
2023-10-05 18:48:28 +0300

    Some fixes

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,9 +1,6 @@
 const text = localStorage.getItem('text');
 let textarea = document.getElementById('editor');
-console.log(localStorage.getItem('text'))
-if (text) {
-    textarea.value = text;
-}
+textarea.value = text;
 function setLocalStorage(key, content) {
     localStorage.setItem(key, content)
 }
@@ -11,9 +8,8 @@ function setLocalStorage(key, content) {
 
 
 
-textarea.addEventListener('change', () => {
+textarea.addEventListener('input', () => {
     if (textarea.value != '') {
-        localStorage.clear();
         setLocalStorage('text', textarea.value)
     }
 }) 
